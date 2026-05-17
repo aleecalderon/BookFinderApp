085635bf787a20c702761dc1d1a14d94a90d7420
aleecalderon <alejandracristal162@gmail.com>
2026-05-17 00:33:33 -0600

    Agregada navegación y nuevas pantallas

---
+++ app/favorites.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function FavoritesScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>❤️ Favoritos</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
